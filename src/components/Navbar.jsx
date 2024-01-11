e14f827a6d2695f7234359b012ef4b738c61907b
Priti <itspriti1999@gmail.com>
2024-01-11 23:20:23 +0530

    2nd commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-      <header className='header'>
-          <NavLink to="/" className="text-xl w-10 h-10 py-1 px-8 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-              <p className='blue-gradient_text'>Priti</p>
-          </NavLink>
-      </header>
-  )
-}
+    <header className="header">
+      <NavLink
+        to="/"
+        className="text-xl w-10 h-10 py-1 px-8 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">PRITI</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+              <NavLink to="/about" className={({isActive})=> isActive?'text-blue-500':'text-black'}>
+                  About
+              </NavLink>
+              <NavLink to="/projects" className={({isActive})=> isActive?'text-blue-500':'text-black'}>
+                  Projects
+              </NavLink>
+              <NavLink to="/contacts" className={({isActive})=> isActive?'text-blue-500':'text-black'}>
+                  Contact
+              </NavLink>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
