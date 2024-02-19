c61b724810cbfc5cc572711c2ad9d2e1ba894204
Priti <itspriti1999@gmail.com>
2024-02-20 00:36:53 +0530

    4th commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,18 @@ const Navbar = () => {
     <header className="header">
       <NavLink
         to="/"
-        className="text-xl w-10 h-10 py-1 px-8 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+        className="text-xl w-10 h-10 py-1 px-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
       >
-        <p className="blue-gradient_text">PRITI</p>
+        <p className="text-red-500">PRITI</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
-              <NavLink to="/about" className={({isActive})=> isActive?'text-blue-500':'text-black'}>
+              <NavLink to="/about" className={({isActive})=> isActive?'text-red-500':'text-red-900 font-semibold'}>
                   About
               </NavLink>
-              <NavLink to="/projects" className={({isActive})=> isActive?'text-blue-500':'text-black'}>
+              <NavLink to="/projects" className={({isActive})=> isActive?'text-red-500':'text-red-900 font-semibold'}>
                   Projects
               </NavLink>
-              <NavLink to="/contacts" className={({isActive})=> isActive?'text-blue-500':'text-black'}>
+              <NavLink to="/contacts" className={({isActive})=> isActive?'text-red-500':'text-red-900 font-semibold'}>
                   Contact
               </NavLink>
       </nav>
